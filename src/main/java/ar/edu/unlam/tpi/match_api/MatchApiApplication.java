a05be12ea9b9164e5639ddc30c2d250f3eae6f9c
Julián Guarnieri <jguarnieri@alumno.unlam.edu.ar>
2025-05-02 20:25:57 -0300

    Base Project

---
+++ src/main/java/ar/edu/unlam/tpi/match_api/MatchApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tpi.match_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchApiApplication.class, args);
+	}
+
+}
